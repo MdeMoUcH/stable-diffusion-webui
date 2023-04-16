263f0984173f8798a32468fa92920d7db31edbae
Live session user <mdemouch@gmail.com>
2023-04-16 17:13:39 +0000

    Apaño de la instalación de las dependencias

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,13 +4,13 @@
 
 sudo apt-get update
 
-sudo apt-get install build-essential wget curl git python3 python3-venv
-	\ libffi-dev openssl libssl-dev liblzo2-dev libpam0g-dev zlib1g-dev
-	\ zlib1g libbz2-dev liblzma-dev libncurses5-dev libreadline6-dev
-	\ libsqlite3-dev libgdbm-dev liblzma-dev lzma lzma-dev libgdbm-dev
-	\ libncursesw5-dev libreadline-dev libxml2-dev libxmlsec1-dev llvm 
-	\ tk-dev xz-utils install libgdbm-compat-dev
-
+sudo apt-get install build-essential wget curl git python3 python3-venv \
+	libffi-dev openssl libssl-dev liblzo2-dev libpam0g-dev zlib1g-dev \
+	zlib1g libbz2-dev liblzma-dev libncurses5-dev libreadline6-dev \
+	libsqlite3-dev libgdbm-dev liblzma-dev lzma lzma-dev libgdbm-dev \
+	libncursesw5-dev libreadline-dev libxml2-dev libxmlsec1-dev llvm  \
+	tk-dev xz-utils libgdbm-compat-dev
+	
 #descargar python 10
 wget https://www.python.org/ftp/python/3.10.9/Python-3.10.9.tgz
 tar-xvzf Python-3.10.9.tgz
