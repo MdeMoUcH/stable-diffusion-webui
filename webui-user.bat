af2a0c2a4f5ecd5f7273bf4d5e545d374c270917
unknown <mdemouch@gmail.com>
2023-04-23 22:56:29 +0200

    Configuración para windows con Python 3.10.6

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,15 @@
 @echo off
 
-set PYTHON=
+set PYTHON=py
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+
+
+set XFORMERS_PACKAGE=xformers==0.0.18
+set TORCH_COMMAND=pip install torch==2.0.0+cu118 torchvision --extra-index-url https://download.pytorch.org/whl/cu118
+
+
+set COMMANDLINE_ARGS=--xformers
+
 
 call webui.bat
