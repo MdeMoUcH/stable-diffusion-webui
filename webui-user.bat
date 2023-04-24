3f547b731ae66b7c3e07a00cf59e5682824dbb18
unknown <mdemouch@gmail.com>
2023-04-24 17:03:43 +0200

    Cambios de la configuración para windows.

--- webui-user.bat
+++ webui-user.bat
@@ -9,7 +9,7 @@ set XFORMERS_PACKAGE=xformers==0.0.18
 set TORCH_COMMAND=pip install torch==2.0.0+cu118 torchvision --extra-index-url https://download.pytorch.org/whl/cu118
 
 
-set COMMANDLINE_ARGS=--xformers
+set COMMANDLINE_ARGS=--xformers --medvram --disable-nan-check
 
 
 call webui.bat
